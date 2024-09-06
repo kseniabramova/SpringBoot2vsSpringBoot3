585b0d9f8112d4d2f5d40717b2b1b3ea0429b7e7
kseniabramova <ksenia.abramova.ka@gmail.com>
2024-09-06 23:45:08 +0200

    SB-1: add dockerfile gradle

---
+++ graalvm/src/main/kotlin/com/example/graalvm/PingController.kt
@@ -0,0 +1,13 @@
+package com.example.graalvm
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PingController {
+
+    @GetMapping("/ping")
+    fun ping(): String {
+        return "pong"
+    }
+}
