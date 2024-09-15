a74c60b5c84cc6d83ba3e067ba7c2a4db11a6329
kseniabramova <ksenia.abramova.ka@gmail.com>
2024-09-15 12:34:36 +0200

    SB-1: test execution time natve vs jvm

--- graalvm/src/main/kotlin/com/example/graalvm/PingController.kt
+++ graalvm/src/main/kotlin/com/example/graalvm/PingController.kt
@@ -1,7 +1,9 @@
 package com.example.graalvm
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 class PingController {
@@ -10,4 +12,28 @@ class PingController {
     fun ping(): String {
         return "pong"
     }
+
+    @GetMapping("/performance")
+    fun testPerformance(): ResponseEntity<String> {
+        val startTime = System.currentTimeMillis()
+
+        // Example of working with memory: sorting a large array
+        val largeArray = generateLargeArray(1000000)
+        Arrays.sort(largeArray)
+
+        val endTime = System.currentTimeMillis()
+        val duration = endTime - startTime
+
+        return ResponseEntity.ok("Execution time: $duration ms")
+    }
+
+    private fun generateLargeArray(size: Int): IntArray {
+        val random: Random = Random()
+        val array = IntArray(size)
+        for (i in 0 until size) {
+            array[i] = random.nextInt()
+        }
+        return array
+    }
+
 }
